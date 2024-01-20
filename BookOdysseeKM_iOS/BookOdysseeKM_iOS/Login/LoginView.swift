40c6cbad8d4864eb74a9da2494efd585b506d78d
Elna Halilovic <elna.halilovic@diamir.io>
2024-01-20 15:30:23 +0100

    add ios views

---
+++ BookOdysseeKM_iOS/BookOdysseeKM_iOS/Login/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  BookOdysseeKM_iOS
+//
+//  Created by Elna on 20.01.24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ BookOdysseeKM_iOS/BookOdysseeKM_iOS/Start/StartView.swift
@@ -0,0 +1,77 @@
+//
+//  StartView.swift
+//  BookOdysseeKM_iOS
+//
+//  Created by Elna on 20.01.24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+
+import SwiftUI
+//import AsyncReactor
+
+struct StartView: View {
+    
+//    @Environment(\.managedObjectContext) private var viewContext
+    
+    var body: some View {
+        NavigationStack {
+            VStack() {
+                Text("Welcome to\nBookOdyssee!")
+                    .font(.largeTitle)
+                    .foregroundStyle(Color("Secondary"))
+                    .padding(.bottom)
+                    .multilineTextAlignment(.center)
+                
+                
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 250, height: 130)
+                    .scaledToFit()
+                    .padding()
+                
+                Text("Your pocket library to go")
+                    .font(.caption)
+                    .foregroundStyle(Color("Secondary"))
+                
+                
+                Group{
+                    NavigationLink(
+                        destination: LoginView()
+//                        destination: ReactorView(LoginReactor(moc: viewContext)) {
+//                            LoginView()
+//                        }
+                    ) {
+                        Text("Login").padding(15).buttonStyle(.borderless)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("Primary"))
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                    }
+                }
+                
+                
+                NavigationLink(
+                    destination: LoginView()
+//                    destination: ReactorView(RegistrationReactor(moc: viewContext)) {
+//                        RegistrationView()
+//                    }
+                ) {
+                    Text("Register")
+                        .padding(15)
+                        .buttonStyle(.borderless)
+                        .frame(maxWidth: .infinity)
+                        .background(Color("Primary"))
+                        .foregroundColor(.white)
+                }
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+        }
+    }
+}
+
+#Preview {
+    StartView()
+}
